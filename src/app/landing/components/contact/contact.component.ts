baa7a71a54ca95270aef00ac9c6c97517c136072
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2025-07-03 20:09:54 -0300

    Update: Finish pricing and contact

--- src/app/landing/components/contact/contact.component.ts
+++ src/app/landing/components/contact/contact.component.ts
@@ -1,11 +1,71 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { LandingRoutingModule } from '../../landing-routing.module';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule,
+    LandingRoutingModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule
+   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+
+mailEnviado = false;
+
+formularioContacto = new FormGroup ({
+  name: new FormControl('', Validators.required),
+  bussines: new FormControl('', Validators.required),
+  email: new FormControl('', [Validators.required, Validators.email]),
+  message: new FormControl('', [Validators.required, Validators.minLength(10)])
+})
+
+// emailService: EmailService= inject(EmailService)
+// crearEmail(): EmailContactoInterface {
+// return{
+//   nombre: this.formularioContacto.get('nombre')?.value || '',
+//   negocio: this.formularioContacto.get('negocio')?.value || '',
+//   email:this.formularioContacto.get('email')?.value || '',
+//   mensaje: this.formularioContacto.get('mensaje')?.value || '',
+// }
+// }
+
+
+// enviarMail() {
+//   const email: EmailContactoInterface = this.crearEmail();
+//   this.emailService.postEnviarEmailContacto(email).subscribe({
+//     next: (response) => {
+//       this.bloquearContacto();
+//     }})
+// }
+
+
+bloquearContacto() {
+  this.mailEnviado = true;
+  this.formularioContacto.get('nombre')?.disable();
+  this.formularioContacto.get('negocio')?.disable();
+  this.formularioContacto.get('email')?.disable();
+  this.formularioContacto.get('mensaje')?.disable();
+  // Swal.fire({
+  //   icon: 'success',
+  //   title: '¡Mail enviado de forma correcta!',
+  //   showConfirmButton: false,
+  //   timer: 1500
+  // })
+  //alert("Mail envíado de forma correcta");
+}
 
 }
--- src/app/landing/pages/landing-page/landing-page.component.ts
+++ src/app/landing/pages/landing-page/landing-page.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import { PreciosComponent } from '../../components/precios/precios.component';
 import { CtaComponent } from '../../components/cta/cta.component';
-import { ContactoComponent } from '../../components/contacto/contacto.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { NavBarLandingComponent } from '../../components/nav-bar-landing/nav-bar-landing.component';
 import { BenefitsComponent } from "../../components/benefits/benefits.component";
 import { IntroComponent } from "../../components/intro/intro.component";
 import { FeaturesComponent } from "../../components/features/features.component";
+import { PricingComponent } from "../../components/pricing/pricing.component";
+import { ContactComponent } from "../../components/contact/contact.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [BenefitsComponent, PreciosComponent, CtaComponent, ContactoComponent, FooterComponent, HeroComponent, NavBarLandingComponent, BenefitsComponent, IntroComponent, FeaturesComponent],
+  imports: [BenefitsComponent, CtaComponent, FooterComponent, HeroComponent, NavBarLandingComponent, BenefitsComponent, IntroComponent, FeaturesComponent, PricingComponent, ContactComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
